b1ca2da51f4769c9bf721d166799e3ca2550f696
PhongPhamdt <phongpham.hedspi@gmail.com>
2019-04-30 14:59:36 +0700

    Fixed Navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.png';
-import NavbarMenu from './components/NavbarMenu'
-import SlideShow from './components/SlideShow'
-import Footer from './components/Footer'
+import React from 'react';
+import Navbar from './components/Navbar';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Section from './components/Section';
 
-class App extends Component {
-  render() {
-    let links = [
-      { label: 'About', link: '#about' },
-      { label: 'Help', link: '#help' },
-      { label: 'Contact', link: '#contact' },
-    ];
-
-    return (
-      <div className="App">
-        <div>
-          <header>
-            <NavbarMenu links={links} logo={logo} />
-          </header>
-
-          <body>
-            <div className="slideshow-container">
-              <SlideShow />
-            </div>
-          </body>
-
-          <footer>
-            <Footer />
-          </footer>
-        </div>
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+      <Header/>
+      <Section/>
+      <Footer/>
+    </div>
+  );
 }
 
 export default App;
